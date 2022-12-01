87ab1a0a56449eb2272142dd2f3d0d74b05f9bd7
ivanovav.24 <ivanov.arseny23@gmail.com>
2022-12-01 12:39:09 +0300

    window title ready

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,12 +1,40 @@
 package app;
 
-import io.github.humbleui.jwm.Event;
+import io.github.humbleui.jwm.*;
 
+import java.io.File;
 import java.util.function.Consumer;
 
 public class Application implements Consumer<Event> {
-    @Override
-    public void accept(Event event) {
+    /**
+     *
+     */
+    private final Window window;
 
+    public Application() {
+        window = App.makeWindow();
+        window.setEventListener(this);
+
+        window.setTitle("Java 2D");
+        window.setWindowSize(900, 900);
+        window.setWindowPosition(100, 100);
+        window.setVisible(true);
+        switch (Platform.CURRENT) {
+            case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
+            case MACOS -> window.setIcon(new File("src/main/resources/macos.icns"));
+        }
+    }
+
+    /**
+     *
+     * @param e the input argument
+     */
+    @Override
+    public void accept(Event e) {
+        if (e instanceof EventWindowClose) {
+            App.terminate();
+        }else if (e instanceof EventWindowCloseRequest) {
+            window.close();
+        }
     }
 }
