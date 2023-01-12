8e1f57b36dd8d9af8a72bc66b4f8f3031490c783
ivanovav.24 <ivanov.arseny23@gmail.com>
2023-01-12 13:08:30 +0300

    log works

--- src/main/java/Panels/PanelLog.java
+++ src/main/java/Panels/PanelLog.java
@@ -1,12 +1,24 @@
 package Panels;
 
+import app.Point;
 import controls.Label;
 import io.github.humbleui.jwm.*;
 import io.github.humbleui.skija.Canvas;
+import io.github.humbleui.skija.FontMetrics;
+import io.github.humbleui.skija.Paint;
 import misc.CoordinateSystem2i;
+import misc.Misc;
+import misc.Vector2d;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import static app.Application.PANEL_PADDING;
 import static app.Colors.PANEL_BACKGROUND_COLOR;
+import static app.Fonts.FONT12;
 
 /**
  * Панель управления
@@ -63,6 +75,111 @@ public class PanelLog extends GridPanel {
      */
     @Override
     public void paintImpl(Canvas canvas, CoordinateSystem2i windowCS) {
-        label.paint(canvas, windowCS);
+        // создаём перо
+        try (Paint paint = new Paint()) {
+            // получаем метрики шрифта
+            FontMetrics metrics = FONT12.getMetrics();
+            // сохраняем область рисования
+            canvas.save();
+            // смещаем область рисования
+            canvas.translate(padding, windowCS.getSize().y - padding - metrics.getDescent());
+            // перебираем записи лога
+            for (int i = logs.size() - 1; i >= 0; --i) {
+                // получаем запись лога
+                Record log = logs.get(i);
+                // задаём цвет лога
+                paint.setColor(getColor(log.recordType));
+                // выводим строку на экран
+                canvas.drawString(log.toString(), 0, 0, FONT12, paint);
+                // смещаем область к следующей линии
+                canvas.translate(0, -metrics.getCapHeight() - 8);
+            }
+            // восстанавливаем область рисования
+            canvas.restore();
+        }
+    }
+    private static final int LOG_LINES_CNT = 15;
+    private static final int MAX_LOG_LINE_LENGTH = 80;
+    enum RecordType {
+        /**
+         * Информация
+         */
+        INFO,
+        /**
+         * Предупреждение
+         */
+        WARNING,
+        /**
+         * Ошибка
+         */
+        ERROR,
+        /**
+         * Успех
+         */
+        SUCCESS
+    }
+    record Record(RecordType recordType, String text, Date date) {
+        /**
+         * Строковое представление объекта
+         *
+         * @return строковое представление объекта
+         */
+        @Override
+        public String toString() {
+            return new SimpleDateFormat("  HH:mm:ss").format(date) + ": " + text;
+        }
+    }
+    private static final List<Record> logs = new ArrayList<>();
+    public static void addToLog(RecordType recordType, String text) {
+        for (String line : text.split("\n")) {
+            for (String limitedLine : Misc.limit(line, MAX_LOG_LINE_LENGTH)) {
+                while (logs.size() > LOG_LINES_CNT)
+                    logs.remove(0);
+                logs.add(new Record(recordType, limitedLine, Calendar.getInstance().getTime()));
+            }
+        }
+    }
+    public static void success(String text) {
+        addToLog(RecordType.SUCCESS, text);
+    }
+
+    /**
+     * Добавить info запись
+     *
+     * @param text текст записи
+     */
+    public static void info(String text) {
+        addToLog(RecordType.INFO, text);
+    }
+
+    /**
+     * Добавить warning запись
+     *
+     * @param text текст записи
+     */
+    public static void warning(String text) {
+        addToLog(RecordType.WARNING, text);
+    }
+
+    /**
+     * Добавить error запись
+     *
+     * @param text текст записи
+     */
+    public static void error(String text) {
+        addToLog(RecordType.ERROR, text);
+    }
+    public static int getColor(RecordType recordType) {
+        return switch (recordType) {
+            case INFO -> Misc.getColor(144, 255, 255, 255);
+            case WARNING -> Misc.getColor(144, 255, 255, 0);
+            case ERROR -> Misc.getColor(144, 255, 0, 0);
+            case SUCCESS -> Misc.getColor(144, 0, 255, 0);
+        };
+    }
+    public void addPoint(Vector2d pos, Point.PointSet pointSet) {
+        Point newPoint = new Point(pos, pointSet);
+        // Добавляем в лог запись информации
+        PanelLog.info("точка " + newPoint + " добавлена в " + newPoint.getSetName());
     }
 }
